0ab053505269dbd3fb78fe9521f2fe84b7f2bcce
RohitJ1995 <59526662+RohitJ1995@users.noreply.github.com>
2022-09-06 12:00:10 +0530

    - add knex migration config - add up and down scripts to create tables

---
+++ db/knexfile.js
@@ -0,0 +1,60 @@
+// Update with your config settings.
+const { knexSnakeCaseMappers } = require('objection')
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'objection',
+      user: 'rohitj',
+      password: null
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    },
+    ...knexSnakeCaseMappers
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
